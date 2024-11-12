db357e817e72cf5dcdb1797c930f728933f68a26
ayasirwhizznet <ayasir@whizznet.com>
2024-11-12 21:14:39 +0500

    Add chevron animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-
+import { PreloaderService } from './preloader.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,6 +10,16 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'Whizz';
+
+  constructor(private preloader: PreloaderService) {}
+
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.preloader.hide();
+  }
 }
