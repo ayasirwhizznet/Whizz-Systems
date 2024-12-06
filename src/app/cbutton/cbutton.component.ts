4d138c39f76b587aeac04c113fb1aa50e0f916d9
ayasirwhizznet <ayasir@whizznet.com>
2024-12-06 12:31:48 +0500

    .

--- src/app/cbutton/cbutton.component.ts
+++ src/app/cbutton/cbutton.component.ts
@@ -18,7 +18,7 @@ export class CbuttonComponent {
   get sizeClass(): string {
     switch (this.size) {
       case 'small':
-        return 'px-[12px] py-[7px] text-sm font-semibold border-[1px] border-teal2';
+        return 'px-[18px] py-[7px] text-sm font-semibold border-[1px] border-teal2';
       case 'medium':
         return 'px-[14px] py-[11px] text-base font-semibold border-[1px] border-teal2'
       case 'large':
