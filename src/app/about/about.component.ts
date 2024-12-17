ffb6be163526a3997cdbe6cc7f2e6e6b9e2ec3c6
ayasirwhizznet <ayasir@whizznet.com>
2024-12-17 23:28:39 +0500

    angular map library issue

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,30 +1,13 @@
 import { Component } from '@angular/core';
 import { CbuttonComponent } from '../cbutton/cbutton.component';
 import { CommonModule } from '@angular/common';
-import { ViewEncapsulation, Inject } from '@angular/core';
-import {
-  MapsTheme,
-  Maps,
-  Legend,
-  Marker,
-  MapsTooltip,
-  ILoadEventArgs,
-  MapsModule,
-} from '@syncfusion/ej2-angular-maps';
-
-Maps.Inject(Legend, Marker, MapsTooltip);
-declare var require: any;
-let worldMap: object[] = require('./world-map.json');
-let datasource: object[] = require('./default-datasource.json');
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CbuttonComponent, CommonModule, MapsModule],
+  imports: [CbuttonComponent, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
-  encapsulation: ViewEncapsulation.None,
-
 })
 
 export class AboutComponent {
@@ -67,127 +50,5 @@ export class AboutComponent {
     { imgUrl: '../../assets/about/Manny-Karim.png', name: 'Manny Karim', designation: 'CEO', desc: 'Manny’s expertise in operations, finance, and manufacturing has been instrumental in Whizz Systems’ success. Starting his career at Pentagon Systems in 1989, Manny co-founded Whizz Systems and developed a robust team and processes ensuring maximum efficiency without compromising quality. He oversees 4 state-of-the-art assembly lines in Santa Clara and 2 facilities in Malaysia.' },
     { imgUrl: '../../assets/about/Muuhammad-Irfan.png', name: 'Muhammad Irfan', designation: 'President', desc: `With a background in product development and manufacturing services, Muhammad co-founded Whizz Systems in 1999. His leadership has driven Whizz Systems' global presence across 3 continents, serving industries like computing, networking, medical, and defense.` },
   ];
-
-  titleSettings: object = {
-    text: 'Global Footprint',
-    textStyle: {
-      size: '16px',
-    },
-  };
-  public zoomSettings: object = {
-    enable: false,
-  };
-  public legendSettings: object = { visible: true };
-
-  public layers: object[] = [
-    {
-      shapeData: worldMap,
-      shapePropertyPath: 'continent',
-      shapeDataPath: 'continent',
-      dataSource: datasource,
-      shapeSettings: { colorValuePath: 'color' },
-      markerSettings: [
-        {
-          visible: true,
-          dataSource: [
-            {
-              latitude: 37.354107,
-              longitude: -121.955238,
-              name: 'Santa Clara, CA, USA',
-            },
-            {
-              latitude: 4.2105,
-              longitude: 101.9758,
-              name: 'Malaysia ',
-            },
-          ],
-          shape: '',
-          //   imageUrl: './assets/maps/images/ballon.png',
-          height: 20,
-          width: 20,
-          tooltipSettings: { visible: true, valuePath: 'name' },
-          animationDuration: 0,
-        },
-        {
-          visible: true,
-          template:
-            `
-          <div style="">
-          <div style='width:15px;height:15px;border-radius:15px;border:4px solid #2C353A;margin-bottom:20px; '>
-          <div/>
-          <div style="position:absolute;left:-65px;top:10px;">
-      
-          <div style="width: 0;
-          height: 0;
-          border-left: 9px solid transparent;
-          border-right: 9px solid transparent;
-          border-bottom: 18px solid #2A494C;"></div>
-          <div style="height:150px;width:120px;background-color:#2C353A; border-radius: 8px; padding: 16px">
-              <p style="font-weight:500; color:white">Santa Clara, CA, USA</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">35,000 Square Foot Facility</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">95 Employees</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">SPI, FP, AOI, X-RAY</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">4 Juki SMT Lines</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">QMS, ITAR</p>
-              <hr style="border: 1px solid #D9D9D9">
-          </div>
-          </div>
-         
-          </div>
-       
-          `,
-          dataSource: [{ latitude: 37.354107, longitude: -121.955238 }],
-          animationDuration: 0,
-        },
-        {
-          visible: true,
-          template:
-            `
-        <div style="">
-          <div style='width:15px;height:15px;border-radius:15px;border:4px solid #2C353A;margin-bottom:20px; '>
-          <div/>
-          <div style="position:absolute;left:-65px;top:10px;">
-      
-          <div style="width: 0;
-          height: 0;
-          border-left: 9px solid transparent;
-          border-right: 9px solid transparent;
-          border-bottom: 18px solid #2A494C;"></div>
-          <div style="height:150px;width:120px;background-color:#2C353A; border-radius: 8px; padding: 16px">
-              <p style="font-weight:500; color:white">Malaysia</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">80,000 Square Foot Facility</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">100 Employees</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">SPI, FP, AOI, X-RAY</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">4 Juki SMT Lines</p>
-              <hr style="border: 1px solid #D9D9D9">
-              <p style="font-weight:300; color:white">QMS</p>
-              <hr style="border: 1px solid #D9D9D9">
-          </div>
-          </div>
-         
-          </div>
-       
-        `,
-          dataSource: [{ latitude: 4.2105, longitude: 101.9758 }],
-          animationDuration: 0,
-        },
-      ],
-    },
-  ];
-
-  // custom code start
-  constructor() { }
-  // custom code end
-
-
 }
 
