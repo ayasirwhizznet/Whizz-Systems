db85f7c58451f1d6779f72e77d67a143a34ab0f7
ayasirwhizznet <ayasir@whizznet.com>
2025-02-05 18:39:20 +0500

    Fix Animations

--- src/app/featured-products/featured-products.component.ts
+++ src/app/featured-products/featured-products.component.ts
@@ -6,278 +6,31 @@ import { Router, RouterModule } from '@angular/router';
   selector: 'app-featured-products',
   standalone: true,
   imports: [CommonModule, RouterModule],
-  templateUrl: './featured-products.component.html',
+  templateUrl: './featured-products.component.html'
 })
 export class FeaturedProductsComponent {
   constructor(private router: Router) {}
-  scrollToCategory(id: string) {
-    const element = document.getElementById(id);
-    if (element) {
-      const offsetPercentage = 30;
-      const offset = (window.innerHeight * offsetPercentage) / 100;
-      const topPosition = element.offsetTop - offset;
-      window.scrollTo({
-        top: topPosition,
-        behavior: 'smooth',
-      });
-    }
+  scrollToFragment(fragment: string): void {
+    setTimeout(() => {
+      const element = document.getElementById(fragment);
+      if (element) {
+        const viewportHeight = window.innerHeight;
+        const offsetPercentage = 42;
+        const offset = (window.innerHeight * offsetPercentage) / 100;
+        const topPosition = element.offsetTop - offset;
+        window.scrollTo({
+          top: topPosition,
+          behavior: 'smooth',
+        });
+      }
+    }, 1);
   }
 
   isEven(arr: any) {
     return arr.length % 2 === 0 ? true : false;
   }
-  featuredProducts: any[] = [
-    {
-      title: 'Evaluations Kit',
-      products: [
-        {
-          name: 'Whizz Kintex',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7 FPGA WH705',
-          link: '/404',
-        },
-        {
-          name: 'USB3/2A F to USB3 Micro B M Adapter',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7 FPGA WS707A',
-          link: '/404',
-        },
-        {
-          name: 'Whizz Zyn',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7000 All Programmable SoC WZ 706',
-          link: '/404',
-        },
-        {
-          name: 'USB3/2A F to USB3 Micro B M Adapter',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7 FPGA VS 707 B',
-          link: '/404',
-        },
-      ],
-    },
-    {
-      title: 'Connectivity Kits',
-      products: [
-        {
-          name: 'Whizz Virtex',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7 FPGA WS709',
-          link: '/404',
-        },
-        {
-          name: 'USB3/2A F to USB3 Micro B M Adapter',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7 FPGA WS 7203A',
-          link: '/404',
-        },
-      ],
-    },
-    {
-      title: 'Characterization Kits',
-      products: [
-        {
-          name: 'Xilinx Kintex',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7 FPGA KC724',
-          link: '/404',
-        },
-        {
-          name: 'Xilinx Virtex',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '7 FPGA WS 7203',
-          link: '/404',
-        },
-      ],
-    },
-    {
-      title: 'Kit Accessories',
-      products: [
-        {
-          name: 'Balun Transformer',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-        {
-          name: 'FMC Loopback Card',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-        {
-          name: 'PCIe loopback Card',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-        {
-          name: 'FMC+ Loopback Card',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-        {
-          name: 'USB3/2A F to USB3 Micro B M Adapter',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-      ],
-    },
-    {
-      title: 'Medical Devices',
-      products: [
-        {
-          name: 'Invensify Insuridge',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-      ],
-    },
-    {
-      title: 'Telecommunication',
-      products: [
-        {
-          name: '5G ORU',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-      ],
-    },
-  ];
 
-  featuredCategories: any = 
-    {
-      'Boards & Kit Accessories': [
-        {
-          title: 'Evaluations Kit',
-          products: [
-            {
-              name: 'Whizz Kintex',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7 FPGA WH705',
-              link: '/404',
-            },
-            {
-              name: 'USB3/2A F to USB3 Micro B M Adapter',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7 FPGA WS707A',
-              link: '/404',
-            },
-            {
-              name: 'Whizz Zyn',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7000 All Programmable SoC WZ 706',
-              link: '/404',
-            },
-            {
-              name: 'USB3/2A F to USB3 Micro B M Adapter',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7 FPGA VS 707 B',
-              link: '/404',
-            },
-          ],
-        },
-        {
-          title: 'Connectivity Kits',
-          products: [
-            {
-              name: 'Whizz Virtex',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7 FPGA WS709',
-              link: '/404',
-            },
-            {
-              name: 'USB3/2A F to USB3 Micro B M Adapter',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7 FPGA WS 7203A',
-              link: '/404',
-            },
-          ],
-        },
-        {
-          title: 'Characterization Kits',
-          products: [
-            {
-              name: 'Xilinx Kintex',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7 FPGA KC724',
-              link: '/404',
-            },
-            {
-              name: 'Xilinx Virtex',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '7 FPGA WS 7203',
-              link: '/404',
-            },
-          ],
-        },
-        {
-          title: 'Kit Accessories',
-          products: [
-            {
-              name: 'Balun Transformer',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '',
-              link: '/404',
-            },
-            {
-              name: 'FMC Loopback Card',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '',
-              link: '/404',
-            },
-            {
-              name: 'PCIe loopback Card',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '',
-              link: '/404',
-            },
-            {
-              name: 'FMC+ Loopback Card',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '',
-              link: '/404',
-            },
-            {
-              name: 'USB3/2A F to USB3 Micro B M Adapter',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '',
-              link: '/404',
-            },
-          ],
-        },
-      ],
-      "Miscellaneous": [
-        {
-          title: 'Medical Devices',
-          products: [
-            {
-              name: 'Invensify Insuridge',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '',
-              link: '/404',
-            },
-          ],
-        },
-        {
-          title: 'Telecommunication',
-          products: [
-            {
-              name: '5G ORU',
-              imgUrl: '../../assets/featured-products/featured-product.png',
-              desc: '',
-              link: '/404',
-            },
-          ],
-        },
-      ],
-    }
-
-  boards:any[] = [
+  boards: any[] = [
     {
       title: 'Evaluations Kit',
       products: [
@@ -362,33 +115,23 @@ export class FeaturedProductsComponent {
           link: '/404',
         },
       ],
-    }
+    },
   ];
 
-  miscellaneous:any[] = [
+  miscellaneous: any[] = [
     {
       title: 'Medical Devices',
-      products: [
-        {
-          name: 'Invensify Insuridge',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-      ],
+      name: 'Invensify Insuridge',
+      imgUrl: '../../assets/featured-products/featured-product.png',
+      desc: '',
+      link: '/404',
     },
     {
       title: 'Telecommunication',
-      products: [
-        {
-          name: '5G ORU',
-          imgUrl: '../../assets/featured-products/featured-product.png',
-          desc: '',
-          link: '/404',
-        },
-      ],
-    }
-  ]
-   
-  
+      name: '5G ORU',
+      imgUrl: '../../assets/featured-products/featured-product.png',
+      desc: '',
+      link: '/404',
+    },
+  ];
 }
