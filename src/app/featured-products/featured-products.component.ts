64db46c476e3b126bc816a75d7e947462b3c1e87
ayasirwhizznet <ayasir@whizznet.com>
2025-02-05 19:29:49 +0500

    Fixed secondary navbar scrolling

--- src/app/featured-products/featured-products.component.ts
+++ src/app/featured-products/featured-products.component.ts
@@ -15,7 +15,7 @@ export class FeaturedProductsComponent {
       const element = document.getElementById(fragment);
       if (element) {
         const viewportHeight = window.innerHeight;
-        const offsetPercentage = 42;
+        const offsetPercentage = 40;
         const offset = (window.innerHeight * offsetPercentage) / 100;
         const topPosition = element.offsetTop - offset;
         window.scrollTo({
