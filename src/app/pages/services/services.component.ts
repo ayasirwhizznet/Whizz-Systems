ef98f8f5f1fec04560e0a03bf84714f1aaac3bd1
ayasirwhizznet <ayasir@whizznet.com>
2025-02-17 02:46:03 +0500

    Set responsiveness of navbar

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -23,7 +23,7 @@ export class ServicesComponent implements OnInit, OnDestroy {
     this.fragmentSubscription = this.route.fragment.subscribe((fragment) => {
       if (fragment) {
         this.currentFragment = fragment;
-        this.scrollToFragment(fragment);
+        this.scrollToCategory(fragment);
       }
     });
 
@@ -33,7 +33,7 @@ export class ServicesComponent implements OnInit, OnDestroy {
         const fragment = this.route.snapshot.fragment;
         if (fragment && fragment !== this.currentFragment) {
           this.currentFragment = fragment;
-          this.scrollToFragment(fragment);
+          this.scrollToCategory(fragment);
         }
       });
   }
@@ -62,11 +62,11 @@ export class ServicesComponent implements OnInit, OnDestroy {
     this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
   }
 
-  scrollToFragment(id: string) {
+  scrollToCategory(id: string) {
     const element = document.getElementById(id);
     if (element) {
       const viewportHeight = window.innerHeight;
-      const offsetPercentage = 35;
+      const offsetPercentage = 42;
       const offset = (viewportHeight * offsetPercentage) / 100;
       const topPosition = element.offsetTop - offset;
       window.scrollTo({
@@ -76,143 +76,143 @@ export class ServicesComponent implements OnInit, OnDestroy {
     }
   }
 
-  servicesTab: any = [
+serviceCategories: any = [
     {
-      title: 'Engineering & Design',
-      description: `With all engineering disciplines in-house—hardware, mechanical, thermal, and power—we deeply understand the complexity and requirements of modern electronics. From concept to production, we design high-performance, manufacturable solutions that meet the toughest industry challenges.`,
-      items: [
+      name: 'Engineering & Design',
+      desc: `With all engineering disciplines in-house—hardware, mechanical, thermal, and power—we deeply understand the complexity and requirements of modern electronics. From concept to production, we design high-performance, manufacturable solutions that meet the toughest industry challenges.`,
+      services: [
         {
-          label: 'System Design/Schematics',
+          name: 'System Design/Schematics',
           link: '/services/engineering_&_design/system_design_&_schematics',
         },
-        { label: 'FPGA Design', link: '/services/engineering_&_design/fpga' },
+        { name: 'FPGA Design', link: '/services/engineering_&_design/fpga' },
         {
-          label: 'PCB Layout',
+          name: 'PCB Layout',
           link: '/services/engineering_&_design/pcb_layout',
         },
         {
-          label: '3D Modeling/Mechanical Engineering',
+          name: '3D Modeling/Mechanical Engineering',
           link: '/services/engineering_&_design/3D_modeling',
         },
         {
-          label: 'Signal Integrity Simulations',
+          name: 'Signal Integrity Simulations',
           link: '/services/engineering_&_design/signal_integrity_simulations',
         },
         {
-          label: 'Power Delivery Network Simulations',
+          name: 'Power Delivery Network Simulations',
           link: '/services/engineering_&_design/power_delivery',
         },
         {
-          label: 'Thermal Management/Thermal Simulation',
+          name: 'Thermal Management/Thermal Simulation',
           link: '/services/engineering_&_design/thermal_management',
         },
       ],
     },
 
     {
-      title: 'NPI',
-      description: `Bringing innovative hardware to market requires precision, efficiency, and deep technical expertise. We bridge the gap between concept and production with a seamless NPI process, ensuring design readiness, manufacturability, and compliance while accelerating time to market.`,
-      items: [
+      name: 'NPI',
+      desc: `Bringing innovative hardware to market requires precision, efficiency, and deep technical expertise. We bridge the gap between concept and production with a seamless NPI process, ensuring design readiness, manufacturability, and compliance while accelerating time to market.`,
+      services: [
         {
-          label: 'System Level Architecture',
+          name: 'System Level Architecture',
           link: '/services/npi',
           fragment: 'SystemLevelArchitecture',
         },
         {
-          label: 'Engineering & Design',
+          name: 'Engineering & Design',
           link: '/services/npi',
           fragment: 'Engineering&Design',
         },
         {
-          label: 'Prototyping',
+          name: 'Prototyping',
           link: '/services/npi',
           fragment: 'Prototyping',
         },
-        { label: 'Testing', link: '/services/npi', fragment: 'Testing' },
+        { name: 'Testing', link: '/services/npi', fragment: 'Testing' },
         {
-          label: 'Compliance and Certification',
+          name: 'Compliance and Certification',
           link: '/services/npi',
           fragment: 'Compliance',
         },
       ],
     },
     {
-      title: 'Advanced Manufacturing',
-      description: `Precision, scalability, and efficiency drive our advanced manufacturing capabilities. Whether assembling complex electronics, integrating mechanical enclosures, or designing system-level packaging, we ensure the highest quality while offering both local and offshore production flexibility.`,
-      items: [
+      name: 'Advanced Manufacturing',
+      desc: `Precision, scalability, and efficiency drive our advanced manufacturing capabilities. Whether assembling complex electronics, integrating mechanical enclosures, or designing system-level packaging, we ensure the highest quality while offering both local and offshore production flexibility.`,
+      services: [
         {
-          label: 'Manufacturing Simulations',
+          name: 'Manufacturing Simulations',
           link: '/services/advanced_manufacturing',
         },
         {
-          label: 'Electronic Manufacturing Assembly',
+          name: 'Electronic Manufacturing Assembly',
           link: '/services/advanced_manufacturing',
         },
         {
-          label: 'Mechanical Assembly',
+          name: 'Mechanical Assembly',
           link: '/services/advanced_manufacturing',
         },
         {
-          label: 'System Build & Packaging Design',
+          name: 'System Build & Packaging Design',
           link: '/services/advanced_manufacturing',
         },
         {
-          label: 'Local & Offshore Capabilities',
+          name: 'Local & Offshore Capabilities',
           link: '/services/advanced_manufacturing',
         },
       ],
     },
     {
-      title: 'Lab Testing Solutions',
-      description:
+      name: 'Lab Testing Solutions',
+      desc:
         'Reliability starts with rigorous validation. Our lab testing solutions ensure every board and system meets industry standards, from initial bring-up to full characterization, so your products perform flawlessly in real-world conditions.',
-      items: [
+        services: [
         {
-          label: 'Board Bring Up',
+          name: 'Board Bring Up',
           link: '/services/lab_testing_solutions',
           fragment: 'BoardBringUp',
         },
         {
-          label: 'Validation',
+          name: 'Validation',
           link: '/services/lab_testing_solutions',
           fragment: 'Validation',
         },
         {
-          label: 'Characterization',
+          name: 'Characterization',
           link: '/services/lab_testing_solutions',
           fragment: 'Characterization',
         },
       ],
     },
     {
-      title: 'Supply Chain Management',
-      description: `Managing electronic components is more complex than ever. Our supply chain solutions provide strategic sourcing, compliance oversight, and procurement expertise to mitigate risk, ensure availability, and keep your production on track.`,
-      items: [
+      name: 'Supply Chain Management',
+      desc: `Managing electronic components is more complex than ever. Our supply chain solutions provide strategic sourcing, compliance oversight, and procurement expertise to mitigate risk, ensure availability, and keep your production on track.`,
+      services: [
         {
-          label: 'Component Engineering',
+          name: 'Component Engineering',
           link: '/services/supply_chain_management',
           fragment: 'ComponentEngineering',
         },
         {
-          label: 'Compliance Engineering',
+          name: 'Compliance Engineering',
           link: '/services/supply_chain_management',
           fragment: 'ComplianceEngineering',
         },
         {
-          label: 'Procurement Engineering',
+          name: 'Procurement Engineering',
           link: '/services/supply_chain_management',
           fragment: 'ProcurementEngineering',
         },
       ],
     },
     {
-      title: 'Sustaining Engineering & Obsolescence Management',
-      description: `Technology evolves, but your product lifecycle doesn’t have to be a limitation. We proactively manage component lifecycles, mitigate EOL risks, and provide alternative sourcing strategies to keep your products viable and competitive.`,
-      items: [
-        { label: 'BOM Management', link: '/404' },
-        { label: 'EOL Management', link: '/404' },
-        { label: 'AVL Management', link: '/404' },
-        { label: 'Second Sourcing/Risk Buying', link: '/404' },
+      name: 'Sustaining Engineering & Obsolescence Management',
+      desc: `Technology evolves, but your product lifecycle doesn’t have to be a limitation. We proactively manage component lifecycles, mitigate EOL risks, and provide alternative sourcing strategies to keep your products viable and competitive.`,
+      services: [
+        { name: 'BOM Management', link: '/404' },
+        { name: 'EOL Management', link: '/404' },
+        { name: 'AVL Management', link: '/404' },
+        { name: 'Second Sourcing/Risk Buying', link: '/404' },
       ],
     },
   ];
