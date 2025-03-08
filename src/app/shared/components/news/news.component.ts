22f69652afc0c127e8afaffe530956a2d8fdfda5
ayasirwhizznet <ayasir@whizznet.com>
2025-03-08 15:24:13 +0500

    Fixed animation

--- src/app/shared/components/news/news.component.ts
+++ src/app/shared/components/news/news.component.ts
@@ -23,31 +23,33 @@ export class NewsComponent implements AfterViewInit {
     {
       imgUrl: 'assets/news/blog-1.png',
       date: 'May 29, 2024',
-      tags: ['Whitepaper','High Speed Connectivity'],
+      tags: ['Whitepaper', 'High Speed Connectivity'],
       title: 'PCIe-6: Everything You Need to Know',
-      link: '/news-&-insights/whitepaper-pcle6'
+      link: '/news-&-insights/whitepaper-pcle6',
     },
     {
       imgUrl: 'assets/news/blog-2.png',
       date: 'July 9, 2024',
-      tags: ['Case Study','Hardware Design'],
-      title: 'Building the Future of 5G Connectivity with Open Radio Unit Solutions',
-      link: '/news-&-insights/whitepaper-5g-oru'
+      tags: ['Case Study', 'Hardware Design'],
+      title:
+        'Building the Future of 5G Connectivity with Open Radio Unit Solutions',
+      link: '/news-&-insights/whitepaper-5g-oru',
     },
     {
       imgUrl: 'assets/news/blog-3.png',
       date: 'July 9, 2024',
-      tags: ['Whitepaper','Thermal Management'],
-      title: 'Heatsinks Uncovered: Best Practices for Optimized Thermal Management',
-      link: '/news-&-insights/whitepaper-heatsink'
+      tags: ['Whitepaper', 'Thermal Management'],
+      title:
+        'Heatsinks Uncovered: Best Practices for Optimized Thermal Management',
+      link: '/news-&-insights/whitepaper-heatsink',
     },
     {
       imgUrl: 'assets/news/blog-4.png',
       date: 'December 12, 2024',
-      tags: ['Case Study','Medical Devices'],
+      tags: ['Case Study', 'Medical Devices'],
       title: 'Engineering Smart Medical Devices: The Invensify Case Study',
-      link: '/news-&-insights/whitepaper-invensify'
-    }
+      link: '/news-&-insights/whitepaper-invensify',
+    },
   ];
 
   @ViewChild('sliderRef') sliderRef = {} as ElementRef;
@@ -60,45 +62,45 @@ export class NewsComponent implements AfterViewInit {
   totalSlides = this.blogs.length;
 
   ngAfterViewInit() {
-    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
-      slideChanged: (slider) => {
-        this.progress = slider.track.details;
-        console.log(this.slider.track.details);
-      },
-
-      breakpoints: {
-        '(min-width: 0px)': {
-          slides: {
-            perView: 1,
-            spacing: 50,
+    setTimeout(() => {
+      this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+        slideChanged: (slider) => {
+          this.progress = slider.track.details;
+        },
+        breakpoints: {
+          '(min-width: 0px)': {
+            slides: {
+              perView: 1,
+              spacing: 50,
+            },
+          },
+          '(min-width: 640px)': {
+            slides: {
+              perView: 2,
+              spacing: 50,
+            },
+          },
+          '(min-width: 1280px)': {
+            slides: {
+              perView: 3,
+              spacing: 50,
+            },
+          },
+          '(min-width: 1536px)': {
+            slides: {
+              perView: 3,
+              spacing: 50,
+            },
+          },
+          '(min-width: 1800px)': {
+            slides: {
+              perView: 3,
+              spacing: 64,
+            },
           },
         },
-        '(min-width: 640px)': {
-          slides: {
-            perView: 2,
-            spacing: 50,
-          },
-        },
-        '(min-width: 1280px)': {
-          slides: {
-            perView: 3,
-            spacing: 50,
-          },
-        },
-        '(min-width: 1536px)': {
-          slides: {
-            perView: 3,
-            spacing: 50,
-          },
-        },
-        '(min-width: 1800px)': {
-          slides: {
-            perView: 3,
-            spacing: 64,
-          },
-        },
-      },
-    });
+      });
+    }, 0);
   }
 
   ngOnDestroy() {
