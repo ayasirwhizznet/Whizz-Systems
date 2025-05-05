b951643ef7a9a52f5d04366bd6bb9b5e4cb32589
ayasirwhizznet <ayasir@whizznet.com>
2025-05-05 15:04:20 +0500

    Add new Testimonial

--- src/app/shared/components/testimonials/testimonials.component.ts
+++ src/app/shared/components/testimonials/testimonials.component.ts
@@ -29,10 +29,10 @@ export class TestimonialsComponent {
       designation: 'VP of Engineering at NVIDIA',
     },
     {
-      desc: 'I have been working with them and again wonderful service, I know what I am going to get, there is no surprises, they deliver on time and their rates are good and they are low cost so I can use them all the time. It’s a just a wonderful relationship and I refer all my clients to them, I refer all my companies to them and will be using them for a long time…',
+      desc: 'Whizz Systems is one of the best design partners I have worked with. Their technical expertise and depth of staff are outstanding. From SOW to execution, Whizz’s project management is always on target. Deadlines and deliverables are consistently met. They have a wide focus, early in the risk retirement phase of the design process, supply chain, and manufacturability are woven into the process. Without hesitation, Whizz Systems will be at the top of the list for a future product development design partner.',
       imgUrl: 'assets/home/testimonials/profile.png',
-      name: 'Neil Mammen',
-      designation: 'Industry Expert',
+      name: 'S. Scott Sober',
+      designation: 'SD of Operations at ENDRA Life Sciences',
     }
   ];
 }
