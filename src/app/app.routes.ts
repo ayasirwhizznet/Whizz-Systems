88b25e62268e048e9f53b2f5dbaabef4e2490397
ayasirwhizznet <ayasir@whizznet.com>
2025-05-06 13:28:41 +0500

    Add Lazy loading on app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,20 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
-import { Page404Component } from './pages/error/404/404.component';
-import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
     title: 'Whizz Systems',
-    data: {description: 'Whizz Systems is a leading provider of innovative electronic design and manufacturing solutions. From concept to production, we deliver high-quality PCB design, prototyping, and assembly services tailored for startups and enterprises alike.'}
-  },  
+    data: {
+      description:
+        'Whizz Systems is a leading provider of innovative electronic design and manufacturing solutions. From concept to production, we deliver high-quality PCB design, prototyping, and assembly services tailored for startups and enterprises alike.',
+    },
+  },
   {
     path: 'about',
-    component: AboutComponent,
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
     title: 'About Us',
   },
   {
@@ -25,12 +27,14 @@ export const routes: Routes = [
       import('./pages/featured-products/routes').then((m) => m.routes),
   },
   {
-    path: 'news-&-insights',
-    loadChildren: () => import('./pages/news-blogs/routes').then((m) => m.routes),
+    path: 'blogs',
+    loadChildren: () =>
+      import('./pages/news-blogs/routes').then((m) => m.routes),
   },
   {
     path: '404',
-    component: Page404Component,
+    loadComponent: () =>
+      import('./pages/error/404/404.component').then((m) => m.Page404Component),
     title: '404 Not Found',
   },
   {
