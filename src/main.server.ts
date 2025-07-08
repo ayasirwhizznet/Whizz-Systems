492e91c9025aba2445b0afcf0d99c4a23dd62b17
ayasirwhizznet <ayasir@whizznet.com>
2025-07-08 14:40:40 +0500

    Remove Server.ts file

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-
-export default function bootstrap() {
-  return bootstrapApplication(AppComponent, appConfig);
-}
